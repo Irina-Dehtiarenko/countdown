26f6944aa9de3819d80a34b8415a6a0b54696620
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-06-23 18:50:15 +0200

    improved style CSS

---
+++ main.js
@@ -0,0 +1,25 @@
+const endTime = new Date('2022-06-29 18:00:00').getTime()
+
+
+
+const spanD = document.querySelector('span.d')
+const spanH = document.querySelector('span.h')
+const spanM = document.querySelector('span.m')
+const spanS = document.querySelector('span.s')
+
+setInterval(() => {
+	const nowTime = new Date().getTime()
+	const time = endTime - nowTime
+
+	const sec = Math.floor((time / 1000) % 60)
+	const min = Math.floor(((time / 1000) / 60) % 60)
+	const hour = Math.floor((((time / 1000) / 60) / 60) % 24)
+	const day = Math.floor((((time / 1000) / 60) / 60) / 24)
+
+
+	spanS.textContent = sec
+	spanM.textContent = min
+	spanH.textContent = hour
+	spanD.textContent = day
+
+}, 1000)
